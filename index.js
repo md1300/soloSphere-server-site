0a858870ba86066880f43149732edd58ebb5585f
md1300 <md.tawhid.khan1998@gmail.com>
2024-10-03 18:39:40 +0600

    jwt include

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 const express=require('express')
 const cors=require('cors')
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cookieParser = require('cookie-parser');
 const port=process.env.PORT|| 9000
 
 const app=express()
 
 const corsOptions={
-    origin:['http://localhost:5173','http://localhost:5174'],
-    Credentials:true,
+    origin:['http://localhost:5173','http://localhost:5174','https://solosphere.web.app'],
+    credentials: true,
     optionSuccessStatus:200,
 }
 app.use(cors(corsOptions))
 app.use(express.json())
+app.use(cookieParser())
+
+
+// verify jwt middleware -----------
+const verifyToken=(req,res,next)=>{
+  const token=req.cookies?.token;
+  if(!token) return res.status(401).send({message:'unauthorized access'})
+  if(token){
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRETS,(error,decoded)=>{
+      if(error){
+        console.log(error)
+        return res.status(401).send({message:'unauthorized access'})
+      }
+      console.log(decoded)
+      req.user=decoded ;
+      next()
+    })
+  }
+  // console.log(token)
+  
+}
+
 
 
 app.get('/',(req,res)=>{
@@ -42,6 +66,30 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
+    // creating jwt token -------------
+
+    app.post('/jwt',async(req,res)=>{
+      const email=req.body;
+      const token=await jwt.sign(email,process.env.ACCESS_TOKEN_SECRETS,{ expiresIn: '365d' })
+      res
+      .cookie('token',token,{
+        httpOnly: true,
+        secure: process.env.NODE_ENV==='production',
+        sameSite: process.env.NODE_ENV==='production'?'none':'strict'
+      })
+      .send({success:true})
+    })
+
+    // clear cookies --------------------
+    app.get('/logOut',(req,res)=>{
+      res.clearCookie('token',{
+        httpOnly:true,
+        secure:process.env.NODE_ENV==='production',
+        sameSite:process.env.NODE_ENV==='production'?'none':'strict',
+        maxAge:0
+      }).send({success:true})
+    })
+
 
  app.get('/jobs',async(req,res)=>{
    const result=await jobsCollection.find().toArray()
@@ -68,8 +116,13 @@ async function run() {
   })
 
   // get my all posted jobs  from mongodb
-  app.get('/jobs/:email',async(req,res)=>{
+  app.get('/jobs/:email',verifyToken, async(req,res)=>{
+    const tokenEmail=req.user.email;
+    // console.log(tokenData,'from token')
     const email=req.params.email ;
+    if(tokenEmail!==email){
+      return res.status(403).send({message:'forbidden access'})
+    }
     const query={'buyer.email':email}
     const result=await jobsCollection.find(query).toArray();
     res.send(result)
@@ -102,8 +155,12 @@ async function run() {
 
     // get my all bids data from bids collection
     
-    app.get('/my-bids/:email',async(req,res)=>{
+    app.get('/my-bids/:email',verifyToken,async(req,res)=>{
       const email=req.params.email;
+      const tokenEmail=req.user.email;
+      if(tokenEmail!==email){
+        return res.status(403).send({message:'forbidden access'})
+      }
       const query={email}
       const result=await bidsCollection.find(query).toArray()
       res.send(result)
@@ -111,8 +168,12 @@ async function run() {
 
     // get all bids requseted data from bids collection
 
-    app.get('/bids-requests/:email',async(req,res)=>{
+    app.get('/bids-requests/:email',verifyToken, async(req,res)=>{
       const email=req.params.email; 
+      const tokenEmail=req.user.email;
+      if(tokenEmail!==email){
+        return res.status(403).send({message:'forbidden access'})
+      }
       const query={'buyer.email':email}
       const result=await bidsCollection.find(query).toArray()
       res.send(result)
